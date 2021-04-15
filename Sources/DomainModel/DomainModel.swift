2a2986702c3f8a14eddab1f8d09e24c98035f3c1
Lauren Ng <2014laurenng@gmail.com>
2021-04-14 17:38:21 -0700

    potatoes

--- Sources/DomainModel/DomainModel.swift
+++ Sources/DomainModel/DomainModel.swift
@@ -9,7 +9,53 @@ struct DomainModel {
 // Money
 //
 public struct Money {
+    
+    var amount: Double
+    var currency: String
+    
+    init(_ total: Int, _ type: String) {
+        if (type == "USD" || type == "GBP" || type == "EUR" || type == "CAN") {
+            amount = Double(total)
+            currency = type
+        } else {
+            amount = 0
+            currency = "Null"
+        }
+    }
+    
+    mutating func convert(_ newCurrency: String) {
+        let newAmount = USD_to_EUR(amount)
+        print("CHANGING US TO EUR")
+        amount = newAmount
+        currency = newCurrency
+    }
+    
+    func add() {
+        
+    }
+    
+    func subtract() {
+        
+    }
+    
+    func USD_to_GBP(_ n: Double) -> Double {
+        print("CHANGING US TO GBP")
+        return 0.5 * n
+    }
+    
+    func USD_to_EUR(_ n: Double) -> Double{
+        print("CHANGING US TO EUR")
+        return 1.5 * n
+    }
+    
+    func USD_to_CAN(_ n: Double) -> Double{
+        print("CHANGING US TO CAN")
+        return 1.25 * n
+    }
 }
+
+let laurenMoney = Money(10, "USD")
+
 
 ////////////////////////////////////
 // Job
